2a48c048b659b18b9d95dfe1245c1115df49a233
Byiringiro Aloys <byiringiroaloys8@gmail.com>
2025-08-19 11:53:19 +0200

    Revert "Attempting to connect to online backend"
    This reverts commit 8e336ad1cf8e458af202c1c526641308a69f0a85.

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 export const config = {
-  apiUrl:'https://ciphera.onrender.com',
+  apiUrl: import.meta.env.VITE_API_URL || 'http://localhost:3560/api',
   appName: 'Ciphera Data Guard',
   version: '1.0.0',
 };
