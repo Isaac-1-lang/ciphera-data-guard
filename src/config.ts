2bb52354b08eba17c4bd303d9ad11aabdf25124b
Byiringiro Aloys <byiringiroaloys8@gmail.com>
2025-08-19 12:11:21 +0200

    configured correct backend url

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 export const config = {
-  apiUrl: import.meta.env.VITE_API_URL || 'http://localhost:3560/api',
+  apiUrl: import.meta.env.VITE_API_URL || 'http://localhost:3560/api' || 'https://ciphera.onrender.com/api',
   appName: 'Ciphera Data Guard',
   version: '1.0.0',
 };
