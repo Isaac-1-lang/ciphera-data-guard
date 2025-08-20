92eb3be94b0e8ee38a80e6792d4901e4913785a2
Byiringiro Aloys <byiringiroaloys8@gmail.com>
2025-08-20 12:06:58 +0200

    Changed backend url

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 export const config = {
-  apiUrl:'https://ciphera-data-guard.onrender.com',
+  apiUrl:'https://ciphera-data-guard.onrender.com/api',
   appName: 'Ciphera Data Guard',
   version: '1.0.0',
 };
